e36ce65558d143974d9bdecefee1ac2a7b53c578
Job Jean-Pierre Joseph <jobjpjoseph@gmail.com>
2024-05-28 06:04:18 -0400

    We finished the assigment

--- solution/dynamic-array.js
+++ solution/dynamic-array.js
@@ -3,17 +3,27 @@ class DynamicArray {
   constructor(defaultSize=4) {
 
     // Your code here
+    this.length = 0;
+    this.capacity = defaultSize;
+    this.data = new Array(defaultSize);
+
+    // for (let i = 0; i < this.capacity; i++) {
+    //   this.data.push('<empty>');
+    // }
 
   }
 
   read(index) {
 
     // Your code here
+    return this.data[index];
   }
 
   unshift(val) {
 
     // Your code here
+    this.data = [val, ...this.data];
+    this.length++;
   }
 
 }
